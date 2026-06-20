ee67882a3e86c12b8c81f5101207595bc5dc227a
AprilTwilight <Katharina.Gambel@edu.fhdw.de>
2026-06-20 10:24:08 +0200

    SpringBoot Projekt erstellt

---
+++ teamverwaltung/teamverwaltung/src/main/java/_interactive/journex/teamverwaltung/TeamverwaltungApplication.java
@@ -0,0 +1,13 @@
+package _interactive.journex.teamverwaltung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamverwaltungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamverwaltungApplication.class, args);
+	}
+
+}
---
+++ teamverwaltung/teamverwaltung/src/test/java/_interactive/journex/teamverwaltung/TeamverwaltungApplicationTests.java
@@ -0,0 +1,13 @@
+package _interactive.journex.teamverwaltung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamverwaltungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
